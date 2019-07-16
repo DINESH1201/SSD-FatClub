4729fd75e5ba12e986f890e6d72d58f2398b18ff
Jun Wei Yue <s10187446@connect.np.edu.sg>
2019-07-16 22:22:08 +0800

    Added input validation to Food Name when creating food.

--- FatClub/Models/Food.cs
+++ FatClub/Models/Food.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace FatClub.Models
 {
     public class Food
     {
         public int ID { get; set; }
+
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "Please enter a proper name. Ensure no special characters or numbers.")]
         public string Name { get; set; }
         public decimal Price { get; set; }
     }
