cbfbecf80dd25702cc1f17c3f2e408151708fb9f
MetaSlave <chongtinghao@gmail.com>
2019-07-17 21:17:12 +0800

    Add Restoran

---
+++ FatClub/Models/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FatClub.Models
+{
+    public class Restaurant
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public List<Food> FoodList { get ; set ; }
+    }
+}
