45d9a447d824880a5dea9a3c4bf838b50f130910
MetaSlave <chongtinghao@gmail.com>
2019-07-17 23:58:41 +0800

    Fixed Stuff

--- FatClub/Pages/Index.cshtml.cs
+++ FatClub/Pages/Index.cshtml.cs
@@ -4,19 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using FatClub.Models;
 
 namespace FatClub.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly FatClub.Models.Restaurant _context;
-        public IndexModel(FatClub.Models.Restaurant context)
+        private readonly FatClub.Models.FatClubContext _context;
+
+        public IndexModel(FatClub.Models.FatClubContext context)
         {
             _context = context;
         }
-
-        [BindProperty(SupportsGet = true)]
-        public string SearchString { get; set; }
 
         public void OnGet()
         {
