53e3dbd120d168563f56f8dab7454dfc5ddf1fad
Dinesh <dinesh1201@live.com.sg>
2019-07-23 14:43:31 +0800

    iasdhiohbkhasdu

---
+++ FatClub/Migrations/20190723064302_updateRating.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FatClub.Migrations
+{
+    public partial class updateRating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Rating");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Rating",
+                maxLength: 150,
+                nullable: true);
+        }
+    }
+}
