8a19d945f4ada51307ec03085ee3195a033645e9
MetaSlave <chongtinghao@gmail.com>
2019-07-24 14:52:05 +0800

    Fixed Shop Cart

--- FatClub/Models/ShoppingCart.cs
+++ FatClub/Models/ShoppingCart.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FatClub.Models
 {
     public class ShoppingCart
     {
+        [ForeignKey("ApplicationUser")]
+        public string UserName { get; set; }
     }
 }
