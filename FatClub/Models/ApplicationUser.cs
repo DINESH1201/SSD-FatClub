f05003ca330497730d606d6e952c817046f05f85
MetaSlave <chongtinghao@gmail.com>
2019-07-26 17:08:53 +0800

    Removed Own Hashing

--- FatClub/Models/ApplicationUser.cs
+++ FatClub/Models/ApplicationUser.cs
@@ -14,9 +14,7 @@ namespace FatClub.Models
         public string FirstName { get; set; }
         [StringLength(50)]
         public string LastName { get; set; }
-        [Required(AllowEmptyStrings = false)]
-        [DisplayFormat(ConvertEmptyStringToNull = false)]
-        public string Salt { get; set; }
+
         public virtual ShoppingCart ShoppingCart { get; set; }
 
     }
