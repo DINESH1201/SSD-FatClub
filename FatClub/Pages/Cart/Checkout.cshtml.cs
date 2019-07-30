01983845b40697e6d51e2574cd25c0c91b6c630b
MetaSlave <chongtinghao@gmail.com>
2019-07-30 17:24:26 +0800

    Ratings should work

--- FatClub/Pages/Cart/Checkout.cshtml.cs
+++ FatClub/Pages/Cart/Checkout.cshtml.cs
@@ -5,15 +5,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using FatClub.Models;
+
 
 namespace FatClub.Pages.Cart
 {
     [Authorize]
     public class CheckoutModel : PageModel
     {
-        public void OnGet()
+        private readonly FatClub.Models.FatClubContext _context;
+        public ViewCartModel(FatClub.Models.FatClubContext context)
         {
-
+            _context = context;
         }
+       
+        public async OnPostAsync()
+        {  
+            var StarRating = Request.Form["starrating"];
+            String currentUsername = User.Identity.Name;
+            ShoppingCart cart = await _context.ShoppingCarts.FirstOrDefaultAsync(m => m.UserName == currentUsername);
+            CartItem cartitem = await _context.CartItems.FirstOrDefaultAsync(item => item.ShoppingCartID == cart.ShoppingCartID);
+            Food food = await _context.Food.FirstOrDefaultAsync(item => item.FoodID == cartitem.FoodID);
+            Restaurant restaurant = await _context.Restaurant.FirstOrDefaultAsync(item => item.RestaurantID == food.RestaurantID);
+
+            var newrating = new Rating() { RestaurantID = restaurant.RestaurantID , Star = StarRating };
+            _context.Rating.Add(newrating);
+        }
+
+
     }
 }
