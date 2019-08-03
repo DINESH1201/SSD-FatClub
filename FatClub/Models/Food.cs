591da9a8a337a0a02cbebe4b6e162fc1cd8ec640
Dinesh <dinesh1201@live.com.sg>
2019-08-03 13:30:23 +0800

    formatting and small change to regex

--- FatClub/Models/Food.cs
+++ FatClub/Models/Food.cs
@@ -8,13 +8,17 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace FatClub.Models
 {
     public class Food
-    {   [Key]
+    {
+        [Key]
         public int FoodID { get; set; }
+
         [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Please enter a proper name. Ensure no special characters or numbers.")]
         public string Name { get; set; }
-        [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})?$", ErrorMessage = "Price cannot have more than 2 decimal places")]
+
+        [RegularExpression(@"^[0-9]+(\.[0-9]{2})?$", ErrorMessage = "Price cannot have more than 2 decimal places")]
         [Column(TypeName = "money")]
         public decimal Price { get; set; }
+
         [Required]
         [ForeignKey("Restaurant")]
         public int RestaurantID { get; set; }
